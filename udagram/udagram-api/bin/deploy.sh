5d17347ed9113d4d60883b68bffdefcc689d8291
hashem <hashem.mohammed@leadliaison.com>
2022-10-17 22:22:20 +0200

    fix: deploy app name issue

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,3 +1,3 @@
 eb init udagram-elastic-app --platform node.js --region us-east-1 &&
-eb use udagram-elastic-app && eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST JWT_SECRET=$JWT_SECRET &&
-eb deploy udagram-elastic-app
+eb use Udagramelasticapp-env && eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST JWT_SECRET=$JWT_SECRET &&
+eb deploy Udagramelasticapp-env
