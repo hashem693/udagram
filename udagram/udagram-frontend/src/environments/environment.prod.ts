b60de897c67de67f579c5e583dd1fc4c5582b0b3
hashem <hashem.mohammed@leadliaison.com>
2022-10-17 22:03:45 +0200

    third commit

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://udagramelasticapp-env.eba-2n3hdd2m.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://udagramelasticapp-env.eba-2n3hdd2m.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
